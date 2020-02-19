353e14bad25df312faa573a0986163079edd7e75
horup <sah@sah.dk>
2020-02-19 07:31:41 +0100

    added hex rendering

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,40 @@
-import { Scene, Engine } from 'babylonjs';
+import { Scene, Engine, FreeCamera, Vector3, Camera, Mesh, HemisphericLight } from 'babylonjs';
 
 let canvas = document.getElementById("render") as HTMLCanvasElement;
 canvas.width = 640;
 canvas.height = 480;
 let engine = new Engine(canvas, false, {preserveDrawingBuffer:true, stencil:true});
+
+const createScene = (e:Engine) =>
+{
+    let size = 10;
+    let scene = new Scene(e);
+    let cam = new FreeCamera('1', new Vector3(0, size, -size), scene);
+    cam.setTarget(new Vector3(0, 0, 0));
+    cam.attachControl(canvas);
+
+    let light = new HemisphericLight('1', new Vector3(0, 1, 0), scene);
+
+    for (let z = 0; z < size; z++)
+    {
+        for (let x = 0; x < size; x++)
+        {
+            let cylinder = Mesh.CreateCylinder('1', 1, 1, 1, 6, 1, scene);
+            cylinder.position.x = x - x / 4;
+            cylinder.position.z = (x % 2 == 0 ? z : z + 0.5) * 0.8;
+            let s = Math.random() * 5;
+            cylinder.scaling.y = s;
+            cylinder.position.y = s / 2;
+        }
+    }
+
+    cam.position.x = 1 + size  / 4;
+    return scene;
+}
+
+let scene = createScene(engine);
+engine.runRenderLoop(()=>
+{
+   
+    scene.render();
+});
