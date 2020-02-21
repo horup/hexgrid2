b221b86b35ee10df461ab2144593f0ac6f08354a
horup <sah@sah.dk>
2020-02-21 07:27:21 +0100

    improved axis handling

--- src/createHexGrid.ts
+++ src/createHexGrid.ts
@@ -1,4 +1,4 @@
-import { Scene, Mesh, HemisphericLight, Vector3 } from "babylonjs";
+import { Scene, Mesh, HemisphericLight, Vector3, Axis, Space, MeshBuilder } from "babylonjs";
 
 /** Creates a hexgrid of the given size */
 export default (size:number, scene:Scene)=>
@@ -8,12 +8,22 @@ export default (size:number, scene:Scene)=>
     {
         for (let x = 0; x < size; x++)
         {
-            let cylinder = Mesh.CreateCylinder('1', 1, 1, 1, 6, 1, scene);
+            let h = 2;
+            let cylinder = MeshBuilder.CreateCylinder("1", {height:1, tessellation:6}, scene);//Mesh.CreateCylinder('1', 1, 1, 1, 6, 1, scene);
             cylinder.rotation.x = Math.PI/2;
-            let s = x + y + 0.1;
+            cylinder.scaling.y = 10;
+            cylinder.position.z = h/2;
+            let s = Math.random()*3 + 0.1;
             cylinder.scaling.y = s;
             cylinder.position.x = x - x / 4;
             cylinder.position.y = (x % 2 == 0 ? y : y + 0.5) * 0.8;
+            cylinder.position.z = cylinder.scaling.y / 2;
         }
     }
+
+    let ground = Mesh.CreateGround("ground", 1, 1, 1, scene);
+    ground.rotate(Axis.X, Math.PI / 2, Space.LOCAL);
+    ground.setPivotPoint(new Vector3(-0.5,0,0.5));
+    ground.scaling.z = size;
+    ground.scaling.x = size;
 }
